fa4702695c3aefea1f645ac2bf55cfcdec7400af
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2022-06-15 15:02:08 +0300

    Add files via upload

--- xmlReader.h
+++ xmlReader.h
@@ -1,21 +1,20 @@
-#ifndef XMLREADER_HPP
-#define XMLREADER_HPP
-
-class XmlReader
-{
-public:
-    XmlReader(const char* filename = "C:/Find_boss_NS_PrIn_267./hierarchy_test.xml") : m_filename(filename) {}
-
-    void xmlReadTitleInfo();
-    void xmlReadSelfInfo();
-    void xmlReadContactInfo();
-    void xmlReadMeetingInfo();
-    void xmlReadMsmServerInfo();
-    void xmlReadAll();
-
-private:
-    const char* m_filename;
-};
-
-
-#endif
+//#ifndef XMLREADER_HPP
+//#define XMLREADER_HPP
+//
+//class XmlReader
+//{
+//public:
+//    XmlReader(const char* filename = "C:/Find_boss_NS_PrIn_267./hierarchy_test.xml") : m_filename(filename) {}
+//
+//    void xmlReadTitleInfo();
+//    void xmlReadSelfInfo();
+//    void xmlReadContactInfo();
+//    void xmlReadMeetingInfo();
+//    void xmlReadMsmServerInfo();
+//    void xmlReadAll();
+//
+//private:
+//    const char* m_filename;
+//};
+//
+//#endif
